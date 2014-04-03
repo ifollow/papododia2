29d493b2bb607ca632aae375d6c98356deebfb65
Antonio Ricardo <renatoauto@ifollowagencia.com.br>
2014-04-03 01:25:50 -0400

    first commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Papododia::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Papododia::Application.config.secret_key_base = '795341120c7f6ff51a4cfec3ba3549416e37567685c65a2e64abc3f396578f80d4c43b004f71a4e8d56181837c04f33ab87624fc7248b94fe9885b6473c461f7'
