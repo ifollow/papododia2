67a1a943f3b719ecd990d149d3f2b5175048e55e
root <root@root.(none)>
2014-04-17 21:53:43 -0400

    precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Papododia::Application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
 
-
+  config.assets.precompile += ['active_admin.css']
 
   # Generate digests for assets URLs.
   config.assets.digest = true
