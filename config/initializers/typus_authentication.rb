c5d40d6014445cb030e62f09dd01a353321f58dc
root <root@root.(none)>
2014-04-17 23:15:04 -0400

    My first commit of app AB

--- config/initializers/typus_authentication.rb
+++ config/initializers/typus_authentication.rb
@@ -1,18 +1,20 @@
 Typus.setup do |config|
 
   # Define authentication: +:none+, +:http_basic+, +:session+
-  config.authentication = :session
-
+  config.authentication = :http_basic
+  
+  config.username = "admin"
+  config.password = "delicia"
   # Define master_role.
-  # config.master_role = "admin"
+  config.master_role = "admin"
 
   # Define relationship.
   # config.relationship = "typus_users"
 
   # Define user_class_name.
-  config.user_class_name = "AdminUser"
+  config.user_class_name = "TypusUser"
 
   # Define user_foreign_key.
-  config.user_foreign_key = "admin_user_id"
+  config.user_foreign_key = "typus_user_id"
 
 end
