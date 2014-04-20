73b77a020759ed1705fe1515361e3a45e84c1754
Antonio Ricardo <renatoauto@ifollowagencia.com.br>
2014-04-20 17:22:14 -0400

    alterações

--- db/schema.rb
+++ db/schema.rb
@@ -14,20 +14,22 @@
 ActiveRecord::Schema.define(version: 20140420083715) do
 
   create_table "admin_users", force: true do |t|
-    t.string   "first_name",       default: "",    null: false
-    t.string   "last_name",        default: "",    null: false
-    t.string   "role",                             null: false
-    t.string   "email",                            null: false
-    t.boolean  "status",           default: false
-    t.string   "token",                            null: false
-    t.string   "salt",                             null: false
-    t.string   "crypted_password",                 null: false
-    t.string   "preferences"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
+  add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "authors", force: true do |t|
     t.string   "name"
